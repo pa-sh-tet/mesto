0eeb461618e6faef8ded1b6e1575f26a65c3d3f0
Pavel Altapov <pavel.altapov@yandex.ru>
2023-05-20 21:02:35 +0300

    до первого ревью

---
+++ index.js
@@ -0,0 +1,38 @@
+//Элементы кнопок
+let editButton = document.querySelector('.profile__edit_button');
+let closeButton = document.querySelector('.popup__close_button');
+let saveButton = document.querySelector('.popup__save_button');
+let popup = document.querySelector('.popup');
+
+//Открытие попапа редактирования профиля
+function openEditProfile () {
+  popup.setAttribute('style', 'display: flex')
+};
+
+//Закрытие попапа редактирования профиля
+function closeEditProfile () {
+  popup.setAttribute('style', 'display: none')
+};
+
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__form');
+
+//Имя и работа в форме
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_description');
+
+//Имя и работа в профиле
+let userName = document.querySelector('.profile__name');
+let userJob = document.querySelector('.profile__description');
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  userName.textContent = nameInput.value;
+  userJob.textContent = jobInput.value;
+  closeEditProfile();
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
+//Срабатывание открытия/закрытия при клике
+editButton.addEventListener('click', openEditProfile);
+closeButton.addEventListener('click', closeEditProfile);
