634632f149985a5e89adbd6c7ef618ee93cac676
Pasha Altapov <2004pablo2004@gmail.com>
2023-09-11 13:43:45 +0300

    refactoring before first review

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,33 +1,33 @@
 export default class UserInfo {
   constructor({nameSelector, descriptionSelector, avatarSelector}) {
-    this._nameSelector = document.querySelector(nameSelector);
-    this._descriptionSelector = document.querySelector(descriptionSelector);
-    this._avatarSelector = document.querySelector(avatarSelector);
+    this._nameElement = document.querySelector(nameSelector);
+    this._descriptionElement = document.querySelector(descriptionSelector);
+    this._avatarElement = document.querySelector(avatarSelector);
   }
 
   //Возвращает объект со значениями, которые сейчас у профиля в разметке
   getUserInfo() {
     return {
-      name: this._nameSelector.textContent,
-      about: this._descriptionSelector.textContent,
-      avatar: this._avatarSelector.src,
+      name: this._nameElement.textContent,
+      about: this._descriptionElement.textContent,
+      avatar: this._avatarElement.src,
     };
   }
 
   //Принимает объект и подставляет его имя и описание в значения профиля в разметку
   setUserInfo({ name, about, avatar, _id }) {
-    this._nameSelector.textContent = name;
-    this._descriptionSelector.textContent = about;
-    this._avatarSelector.src = avatar;
+    this._nameElement.textContent = name;
+    this._descriptionElement.textContent = about;
+    this._avatarElement.src = avatar;
     this._userId = _id;
   }
 
   setProfileInfo({ name, about }) {
-    this._nameSelector.textContent = name;
-    this._descriptionSelector.textContent = about;
+    this._nameElement.textContent = name;
+    this._descriptionElement.textContent = about;
   }
 
   setUserAvatar(avatarLink) {
-    this._avatarSelector.src = avatarLink;
+    this._avatarElement.src = avatarLink;
   }
 }
